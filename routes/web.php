482b0535c1d047182c164bd751b194c3d3d17862
Difelice-e <difelice.emanuele@gmail.com>
2022-05-09 19:27:10 +0200

    edit file front.js e admin.js

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::middleware('auth')
         Route::resource('users', 'UserController');
 
         Route::get('/users/{user}/posts', 'UserPostController@index');
+});
 
+Route::fallback(function(){
+    return view('guest.home');
 });
 
